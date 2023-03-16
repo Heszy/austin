50000f92aa860e106c4948ff66f1f914793bf60f
Heszy <hszy2000@gmail.com>
2023-03-16 10:56:43 +0800

    3.接入日志框架

--- austin-web/src/main/java/com/hszy/austin/web/TestController.java
+++ austin-web/src/main/java/com/hszy/austin/web/TestController.java
@@ -1,13 +1,17 @@
 package com.hszy.austin.web;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class TestController {
 
     @RequestMapping("/test")
     private String test(){
-        return "test";
+        System.out.println("sout打印信息");
+        log.info("使用log打印日志");
+        return("日志");
     }
 }
